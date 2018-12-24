243ebb5a89ff4a19bf96899adce765a5a87119fa
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-12-24 09:55:25 +0200

    configured material design

---
+++ scripts/material.js
@@ -0,0 +1,9 @@
+// mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+
+
+// const tabIndicator = new MDCTabIndicator(document.querySelector('.mdc-tab-indicator'));
+// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+// const topAppBar = new MDCTopAppBar(topAppBarElement);
+mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+mdc.tabBar.MDCTabBar.attachTo(document.querySelector(".mdc-tab-bar"));
+// const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
