71bede589a6674a39229b7c1a250162418781c6e
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-12-28 13:38:56 +0200

    Created options on the side panel and created marker to show users location

--- public/scripts/emergencyServices.js
+++ public/scripts/emergencyServices.js
@@ -5,11 +5,32 @@ let place =  {lat: -29.3151, lng: 27.4869};
 let serviceType = window.location.href;
 let index = serviceType.indexOf('search=');
 serviceType = serviceType.substring(index+7);
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: place, //TODO: Center to be user's location if GPS enabled.
+        center: place, //DONE: Center to be user's location if GPS enabled.
         zoom: 15,
     });
+    infoWindow = new google.maps.InfoWindow;
+// Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
         location: place,
@@ -17,6 +38,7 @@ function initMap() {
         type: [serviceType]
     }, callback);
 }
+
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (let i = 0; i < results.length; i++) {
@@ -37,6 +59,15 @@ function createMarker(place) {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
     });
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
 // TODO: Add directions from user's location to Emergency Service
 // TODO: Give estimates of the time it would take for the user to arrive at emergency service with different modes of transport
