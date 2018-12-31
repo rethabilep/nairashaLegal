1875d7f17bbcb742c7668efc5e251b6a022f9ebd
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-12-31 10:42:59 +0200

    built static files for the deploy

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,15 +1,105 @@
-let map;
-let service;
-let infowindow;
+let map, service, infowindow;
 let place =  {lat: -29.3151, lng: 27.4869};
 let serviceType = window.location.href;
 let index = serviceType.indexOf('search=');
 serviceType = serviceType.substring(index+7);
-
+const mapStyle = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2e5d4"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5dac6"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5c6c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e4d7c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#fbfaf7"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#acbcc9"
+            }
+        ]
+    }
+];
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: place, //DONE: Center to be user's location if GPS enabled.
         zoom: 15,
+        styles: mapStyle,
     });
     infoWindow = new google.maps.InfoWindow;
 // Try HTML5 geolocation.
@@ -68,7 +158,11 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
-}
+};
+
+
+// TODO: Create a database((JSON) of all health services, police stations and half-way homes in Lesotho
+// TODO: Find/Create appropriate images/icons to represent health services, police stations and half-way homes
 // TODO: Add directions from user's location to Emergency Service
 // TODO: Give estimates of the time it would take for the user to arrive at emergency service with different modes of transport
 // TODO: Read user's GPS location or allow user to enter their location
