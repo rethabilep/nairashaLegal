7bc7cfa867ec4bd5b8355ac31930d0cb7d241fe6
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-12-31 15:27:21 +0200

    directions through different modes of travel

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,4 +1,4 @@
-let map, service, infoWindow, userPos , directionsDisplay, directionsService;
+let map, service, userPos, infoWindow, directionsDisplay, directionsService, places;
 let serviceType = window.location.href;
 let index = serviceType.indexOf('search=');
 serviceType = serviceType.substring(index+7);
@@ -105,14 +105,12 @@ function initMap() {
         styles: mapStyle,
     });
     infoWindow = new google.maps.InfoWindow;
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             userPos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
-
             infoWindow.setPosition(userPos);
             infoWindow.setContent('You are here.');
             infoWindow.open(map);
@@ -120,11 +118,11 @@ function initMap() {
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
+
     } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
-
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
         location: {lat: -29.3151, lng: 27.4869},
@@ -135,26 +133,7 @@ function initMap() {
 }
 
 function callback(results, status) {
-    infoWindow = new google.maps.InfoWindow;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            let userPos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(userPos);
-            infoWindow.setContent('You are here.');
-            infoWindow.open(map);
-            map.setCenter(userPos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    places = results;
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (let i = 0; i < results.length; i++) {
             let place = results[i];
@@ -163,27 +142,24 @@ function callback(results, status) {
     }
 }
 
+
 function createMarker(place, userPos) {
     let placeLoc = place.geometry.location;
     let marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
     });
-    let name = place.name;
-    // google.maps.event.addListener(marker, 'click', function() {
-    //     infowindow.setContent(place.name);
-    //     infowindow.open(map, this);
-    // });
-    google.maps.event.addListener(marker, "click", (event)=>{
+    marker.addListener('click', function(){
         enterPlace(place);
-        calculateAndDisplayRoute(directionsService, directionsDisplay, userPos, placeLoc);
+        calculateAndDisplayRoute(userPos, placeLoc);
+        eventFire(document.getElementById('DRIVING'),'click');
         eventFire(document.getElementById('starting-point'), 'focus');
         eventFire(document.getElementById('starting-point'), 'blur');
         eventFire(document.getElementById('destination'), 'focus');
         eventFire(document.getElementById('destination'), 'blur');
+        map.setZoom(15);
     });
 }
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
@@ -199,14 +175,11 @@ function enterPlace(place) {
 
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination) {
-    const selectedMode = "DRIVING";
+function calculateAndDisplayRoute(origin, destination, selectedMode="DRIVING") {
+    console.log(origin);
     directionsService.route({
-        origin: origin,  // Haight.
-        destination: destination,  // Ocean Beach.
-        // Note that Javascript allows us to access the constant
-        // using square brackets and a string value as its
-        // "property."
+        origin: origin,  // User's input
+        destination: destination,  // Selected destination
         travelMode: google.maps.TravelMode[selectedMode]
     }, function(response, status) {
         if (status === 'OK') {
@@ -227,10 +200,20 @@ function eventFire(el, etype){
     }
 }
 
+$(".travel_mode").click(function () {
+    let selectedMode = this.id;
+    let placeLoc;
+    for (let i =0; i<places.length; i++){
+        if (places[i].name === $("#destination").val()) {
+            placeLoc = places[i].geometry.location;
+        }
+    }
+    calculateAndDisplayRoute(userPos, placeLoc, selectedMode);
+});
+
 // TODO: Create a database((JSON) of all health services, police stations and half-way homes in Lesotho
 // TODO: Find/Create appropriate images/icons to represent health services, police stations and half-way homes
 // TODO: Add directions from user's location to Emergency Service
 // TODO: Give estimates of the time it would take for the user to arrive at emergency service with different modes of transport
 // TODO: Read user's GPS location or allow user to enter their location
 // TODO: Show markers of all chosen emrgency service on the map.
-
